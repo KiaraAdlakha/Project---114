3ef39e626bf9820396d213d7364829c54b4a0850
KiaraAdlakha <75115595+KiaraAdlakha@users.noreply.github.com>
2021-10-15 15:44:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload()
+{
+
+}
+function setup()
+{
+canvas=createCanvas(300,300);
+canvas.position(800,200);
+video=createCapture(VIDEO);
+video.hide();
+}
+function draw()
+{
+background("yellow");
+}
